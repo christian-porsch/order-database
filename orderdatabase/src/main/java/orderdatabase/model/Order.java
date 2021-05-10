8fbbf6a8e439710ef5edcbcf864a7e8b55d19994
christianp <porsc.chr@gmail.com>
2021-05-10 20:35:32 +0200

    Add variables for class order with getter, setter and contructor

--- orderdatabase/src/main/java/orderdatabase/model/Order.java
+++ orderdatabase/src/main/java/orderdatabase/model/Order.java
@@ -1,4 +1,21 @@
 package orderdatabase.model;
 
 public class Order {
+
+    private String id;
+    private String name;
+
+    public Order(String id, String name){
+        this.id = id;
+        this.name = name;
+
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
 }

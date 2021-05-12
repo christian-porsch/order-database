bfac93ff39371dcdd6ebb19e479e29e446323038
christianp <porsc.chr@gmail.com>
2021-05-12 08:54:09 +0200

    Add methods to find, list and add orders to class OrderDatabase

--- orderdatabase/src/main/java/orderdatabase/repository/OrderDatabase.java
+++ orderdatabase/src/main/java/orderdatabase/repository/OrderDatabase.java
@@ -1,4 +1,23 @@
 package orderdatabase.repository;
 
+import orderdatabase.model.Order;
+
+import java.util.*;
+
 public class OrderDatabase {
+
+    private Map<String,Order> orders = new HashMap<>();
+
+
+    public Map<String,Order> listAllOrders(){
+        return orders;
+    }
+
+    public Optional<Order> findOrderByID(String id){
+        return Optional.ofNullable(orders.get(id));
+    }
+
+    public void addOrder(Order newOrder){
+        orders.put(newOrder.getId(), newOrder);
+    }
 }
